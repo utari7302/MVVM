156df64f27c27e586e3bc79b9b27d0c3ae1351e2
MUHAMMAD USAMA <utari7302@gmail.com>
2021-04-01 23:46:58 +0500

    Initial commit

---
+++ app/src/main/java/com/dejure/mvvm/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dejure.mvvm.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dejure.mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
